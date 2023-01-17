179163c707a4fb2f6ebcad13417b049c5a997788
mingun choi <ryanchoi100826@gmail.com>
2023-01-17 11:29:50 -0500

    first commit

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+const printSentence = sentence => {
+  for (const char in sentence) {
+    let letter = sentence[char];
+    setTimeout(() => {
+      process.stdout.write(letter);
+    }, char * 50);
+  }
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, sentence.length * 50);
+};
+
+printSentence(sentence);
